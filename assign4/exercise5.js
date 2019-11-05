bf3caf36fef5d6eeb6734ee4655c17615f03f158
Chad Tolleson <tolleson@pdx.edu>
2019-11-05 13:31:53 -0800

    removed old version of exercise 3 and alternative calculation ofor exercise 5

--- assign4/exercise5.js
+++ assign4/exercise5.js
@@ -6,6 +6,7 @@ document.getElementById("bckgrnd").onclick = function() { ChangeBackgroundColor(
 
 function ChangeBackgroundColor() {
     newcolor = "rgb(" + (Math.random() * 255) +"," + (Math.random() * 255) + "," + (Math.random() * 255) + ")";
+    // newcolor = "#"+((1<<24)*Math.random()|0).toString(16);
     document.body.style.backgroundColor = newcolor;
-    console.log("changing background color");
+    console.log("changing background color to "+newcolor);
 }
