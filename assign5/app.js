bc50290174fb3c98b7c532f01685ba982636caf0
Chad Tolleson <tolleson@pdx.edu>
2019-11-13 11:55:02 -0800

    updated version of assignment 5

--- assign5/app.js
+++ assign5/app.js
@@ -1,16 +1,10 @@
 const express = require('express');
-// const form = require('./form');
 const server = express();
 const port = 8080;
-server.use(express.static(__dirname));
-
 const bodyParser = require("body-parser");
-server.use(bodyParser.urlencoded({ extended: false }));
-// server.use(Express.urlencoded({ extended: true }));
 
-
-
-
+server.use(express.static(__dirname));
+server.use(bodyParser.urlencoded({ extended: true }));
 
 server.get('/', function(req, res) {
     res.status(200);
@@ -20,25 +14,16 @@ server.get('/', function(req, res) {
     // res.end();
 });
 
-// server.get('/submit',function(req,res) {
-//     res.send('submit Get Request')
-//     console.log('submit get request in log');
-// });
-
 server.post('/submit', function(req, res) {
-    console.log(req);
-    var name2 = req.body.name;
-    // var email2 = req.body.email;
-    // var feed2 = req.body.feedback;
+    // console.log(req);
     console.log('posting form data!');
     console.log(req.body.name);
-    // console.log(req.body.email);
-    // console.log(req.body.feedback);
+    console.log(req.body.email);
+    console.log(req.body.feedback);
     res.write('Post Request\n');
-    res.write(`name: ${req.body.name}`);
-    // res.write(`e-mail: ${req.body.email}`);
-    // res.write(`feedback: ${req.body.feedback}`);
-    res.send(name2);
+    res.write(`name: ${req.body.name}\n`);
+    res.write(`e-mail: ${req.body.email}\n`);
+    res.write(`feedback: ${req.body.feedback}\n`);
     // next();
     res.end();
 });
